21bdb9db0f9d991c02f6de831aefe5d52297d75f
stroller <fujian1115@gmail.com>
2025-10-04 09:21:53 +0800

    oci support: add oci support for basic operation

---
+++ storage/s3/src/main/java/io/aiven/kafka/tieredstorage/storage/s3/OciStorage.java
@@ -0,0 +1,140 @@
+/*
+ * Copyright 2023 Aiven Oy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.aiven.kafka.tieredstorage.storage.s3;
+
+import com.oracle.bmc.model.BmcException;
+import com.oracle.bmc.model.Range;
+import com.oracle.bmc.objectstorage.ObjectStorageClient;
+import com.oracle.bmc.objectstorage.requests.DeleteObjectRequest;
+import com.oracle.bmc.objectstorage.requests.GetObjectRequest;
+import io.aiven.kafka.tieredstorage.storage.BytesRange;
+import io.aiven.kafka.tieredstorage.storage.InvalidRangeException;
+import io.aiven.kafka.tieredstorage.storage.KeyNotFoundException;
+import io.aiven.kafka.tieredstorage.storage.ObjectKey;
+import io.aiven.kafka.tieredstorage.storage.StorageBackend;
+import io.aiven.kafka.tieredstorage.storage.StorageBackendException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class OciStorage implements StorageBackend {
+
+    ObjectStorageClient objectStorageClient;
+
+    private String bucketName;
+    private int partSize;
+
+    @Override
+    public void configure(final Map<String, ?> configs) {
+        final OciStorageConfig config = new OciStorageConfig(configs);
+        this.objectStorageClient = OciClientBuilder.build(config);
+        this.bucketName = config.bucketName();
+        this.partSize = config.uploadPartSize();
+    }
+
+    @Override
+    public long upload(final InputStream inputStream, final ObjectKey key) throws StorageBackendException {
+        final var out = ociOutputStream(key);
+        try (out) {
+            inputStream.transferTo(out);
+        } catch (final IOException e) {
+            throw new StorageBackendException("Failed to upload " + key, e);
+        }
+        // getting the processed bytes after close to account last flush.
+        return out.processedBytes();
+    }
+
+    OciUploadOutputStream ociOutputStream(final ObjectKey key) {
+        return new OciUploadOutputStream(bucketName, key, partSize, objectStorageClient);
+    }
+
+    @Override
+    public void delete(final ObjectKey key) throws StorageBackendException {
+        try {
+            final DeleteObjectRequest deleteRequest = DeleteObjectRequest.builder()
+                    .bucketName(bucketName)
+                    .objectName(key.value())
+                    .build();
+            objectStorageClient.deleteObject(deleteRequest);
+        } catch (final BmcException e) {
+            throw new StorageBackendException("Failed to delete " + key, e);
+        }
+    }
+
+    @Override
+    public void delete(final Set<ObjectKey> keys) throws StorageBackendException {
+        final List<ObjectKey> objectKeys = new ArrayList<>(keys);
+        //OCI don't support batch delete
+        for (ObjectKey objectKey : objectKeys) {
+            this.delete(objectKey);
+        }
+    }
+
+    @Override
+    public InputStream fetch(final ObjectKey key) throws StorageBackendException {
+        final GetObjectRequest getRequest = GetObjectRequest.builder().bucketName(bucketName).objectName(key.value()).build();
+        try {
+            return objectStorageClient.getObject(getRequest).getInputStream();
+        } catch (final BmcException e) {
+            if (e.getStatusCode() == 404) {
+                throw new KeyNotFoundException(this, key, e);
+            } else {
+                throw new StorageBackendException("Failed to fetch " + key, e);
+            }
+        }
+    }
+
+    @Override
+    public InputStream fetch(final ObjectKey key, final BytesRange range) throws StorageBackendException {
+        try {
+            if (range.isEmpty()) {
+                return InputStream.nullInputStream();
+            }
+            
+            final GetObjectRequest getRequest = GetObjectRequest.builder()
+                .bucketName(bucketName)
+                .objectName(key.value())
+                .range(formatRange(range))
+                .build();
+            return objectStorageClient.getObject(getRequest).getInputStream();
+        } catch (final BmcException e) {
+            if (e.getStatusCode() == 404) {
+                throw new KeyNotFoundException(this, key, e);
+            }
+            if (e.getStatusCode() == 416) {
+                throw new InvalidRangeException("Invalid range " + range, e);
+            }
+
+            throw new StorageBackendException("Failed to fetch " + key, e);
+        }
+    }
+
+    private Range formatRange(final BytesRange range) {
+        return new Range((long)range.firstPosition(), (long) range.lastPosition());
+    }
+
+    @Override
+    public String toString() {
+        return "OciStorage{"
+            + "bucketName='" + bucketName + '\''
+            + ", partSize=" + partSize
+            + '}';
+    }
+}
---
+++ storage/s3/src/main/java/io/aiven/kafka/tieredstorage/storage/s3/OciStorageConfig.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2023 Aiven Oy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.aiven.kafka.tieredstorage.storage.s3;
+
+import com.oracle.bmc.Region;
+import com.oracle.bmc.auth.AbstractAuthenticationDetailsProvider;
+import com.oracle.bmc.auth.InstancePrincipalsAuthenticationDetailsProvider;
+import org.apache.kafka.common.config.AbstractConfig;
+import org.apache.kafka.common.config.ConfigDef;
+
+import java.util.Map;
+
+public class OciStorageConfig extends AbstractConfig {
+
+    public static final String OCI_BUCKET_NAME_CONFIG = "oci.bucket.name";
+    private static final String OCI_BUCKET_NAME_DOC = "oci bucket to store log segments";
+    public static final String OCI_REGION_CONFIG = "oci.region";
+    private static final String OCI_REGION_DOC = "OCI region where object storage bucket is placed";
+
+    private static final String OCI_MULTIPART_UPLOAD_PART_SIZE_CONFIG = "oci.multipart.upload.part.size";
+    private static final String OCI_MULTIPART_UPLOAD_PART_SIZE_DOC = "Size of parts in bytes to use when uploading. "
+        + "All parts but the last one will have this size. "
+        + "The smaller the part size, the more calls to oci are needed to upload a file; increasing costs. "
+        + "The higher the part size, the more memory is needed to buffer the part. "
+        + "Valid values: between 5MiB and 2GiB";
+    static final int OCI_MULTIPART_UPLOAD_PART_SIZE_MIN = 5 * 1024 * 1024; // 5MiB
+    static final int OCI_MULTIPART_UPLOAD_PART_SIZE_MAX = Integer.MAX_VALUE;
+    static final int OCI_MULTIPART_UPLOAD_PART_SIZE_DEFAULT = 25 * 1024 * 1024; // 25MiB
+
+    public static ConfigDef configDef() {
+        return new ConfigDef()
+            .define(
+                    OCI_BUCKET_NAME_CONFIG,
+                ConfigDef.Type.STRING,
+                ConfigDef.NO_DEFAULT_VALUE,
+                new ConfigDef.NonEmptyString(),
+                ConfigDef.Importance.HIGH,
+                    OCI_BUCKET_NAME_DOC)
+            .define(
+                    OCI_REGION_CONFIG,
+                ConfigDef.Type.STRING,
+                ConfigDef.NO_DEFAULT_VALUE,
+                ConfigDef.Importance.MEDIUM,
+                    OCI_REGION_DOC)
+            .define(
+                    OCI_MULTIPART_UPLOAD_PART_SIZE_CONFIG,
+                ConfigDef.Type.INT,
+                    OCI_MULTIPART_UPLOAD_PART_SIZE_DEFAULT,
+                ConfigDef.Range.between(OCI_MULTIPART_UPLOAD_PART_SIZE_MIN, OCI_MULTIPART_UPLOAD_PART_SIZE_MAX),
+                ConfigDef.Importance.MEDIUM,
+                    OCI_MULTIPART_UPLOAD_PART_SIZE_DOC);
+    }
+
+    public OciStorageConfig(final Map<String, ?> props) {
+        super(configDef(), props);
+    }
+
+    Region region() {
+        return Region.fromRegionId(getString(OCI_REGION_CONFIG));
+    }
+
+    AbstractAuthenticationDetailsProvider credentialsProvider() {
+        return InstancePrincipalsAuthenticationDetailsProvider.builder().build();
+    }
+
+    public String bucketName() {
+        return getString(OCI_BUCKET_NAME_CONFIG);
+    }
+
+    public int uploadPartSize() {
+        return getInt(OCI_MULTIPART_UPLOAD_PART_SIZE_CONFIG);
+    }
+
+}
